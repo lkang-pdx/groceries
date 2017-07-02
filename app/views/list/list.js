7e93635df236bcb885782d5b018ba06d0998a102
Luna Kang <luna.kang.ca@gmail.com>
2017-07-01 17:53:41 -0700

    ActivityIndicator

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -15,7 +15,10 @@ exports.loaded = function(args) {
     page.bindingContext = pageData;
 
     groceryList.empty();
-    groceryList.load();
+    pageData.set("isLoading", true);
+    groceryList.load().then(function() {
+        pageData.set("isLoading", false);
+    });
 };
 
 exports.add = function() {
